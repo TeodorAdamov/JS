3785abe7cef6ab78600675bfd98629041a1ba603
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2023-05-17 14:06:18 +0300

    Update 10-pyramidOfKingDjoser.js

--- fundamentals/01-basicSyntax/exercise/10-pyramidOfKingDjoser.js
+++ fundamentals/01-basicSyntax/exercise/10-pyramidOfKingDjoser.js
@@ -1,5 +1,39 @@
-function kingDjoser(cm, increment) {
+function kingDjoser(base, increment) {
+    let step = 1;
+    let stone = 0;
+    let marble = 0;
+    let lapis = 0;
+    let gold = 0;
+    let height = 1;
+
+    for (let curFloor = base; curFloor >= 0; curFloor -= 2){
+        if (curFloor === 2){
+            gold += 2 * 2 * increment;
+            break;
+        } else if (curFloor === 1){
+            gold += 1 * 1 * increment;
+            break;
+        }
+
+        stone += (curFloor - 2) * (curFloor - 2) * increment;
+
+        if (step === 5) {
+            lapis += (curFloor * 4 - 4) * increment;
+            step = 0;
+        } else {
+            marble += (curFloor * 4 - 4) * increment;
+        }
+        step++;
+        height++;
+
+    }
+
+    console.log(`Stone required: ${Math.ceil(stone)}`);
+    console.log(`Marble required: ${Math.ceil(marble)}`);
+    console.log(`Lapis Lazuli required: ${Math.ceil(lapis)}`);
+    console.log(`Gold required: ${Math.ceil(gold)}`);
+    console.log(`Final pyramid height: ${Math.floor(height * increment)}`);
 
 }
 
-kingDjoser()
+kingDjoser(23,0.5)
