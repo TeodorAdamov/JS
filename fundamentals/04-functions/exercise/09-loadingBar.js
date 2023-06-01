f210f40677adb428c3980881d8982b4e42f74101
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2023-06-01 11:49:04 +0300

    loading bar

---
+++ fundamentals/04-functions/exercise/09-loadingBar.js
@@ -0,0 +1,15 @@
+function loadingBar(num) {
+    if (num === 100) {
+        return '100% Complete!';
+    }
+    let arr = Array(10).fill('.')
+
+    for (let i = 0; i < num / 10; i++) {
+        arr[i] = '%';
+    }
+    return `${num}% [${arr.join('')}]\nStill loading...`;
+
+
+}
+
+console.log(loadingBar(100))
