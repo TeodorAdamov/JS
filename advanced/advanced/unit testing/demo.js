11dcf69f8203977eb04c2ebdc0ea65462deb30c1
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2023-10-01 09:48:50 +0300

    advanced functions

---
+++ advanced/advanced/unit testing/demo.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+
+function sum(a, b) {
+    return a + b;
+}
+
+
+describe('test', function () {
+
+    it('works', function () {
+        expect(sum(3,5)).to.equal(9)
+    })
+})
