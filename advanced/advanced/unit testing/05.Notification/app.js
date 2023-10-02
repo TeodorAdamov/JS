9f2847042e89e66c6a7b5307e0d13f99ebf10299
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2023-10-02 10:59:16 +0300

    unit tests homework

--- advanced/advanced/unit testing/05.Notification/app.js
+++ advanced/advanced/unit testing/05.Notification/app.js
@@ -1,4 +1,11 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+    const notificationDiv = document.getElementById('notification');
+
+
+    notificationDiv.textContent = message;
+    notificationDiv.style.display = 'block'
+
+    notificationDiv.addEventListener('click', () => {
+        notificationDiv.style.display = 'none'
+    })
 }
--- advanced/advanced/unit testing/06.Dynamic-Validation/app.js
+++ advanced/advanced/unit testing/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    const input = document.getElementById('email');
+
+    const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/
+
+    input.addEventListener('change', () => {
+        if (input.value.match(pattern) === null) {
+            input.className = 'error'
+        } else {
+            input.className = '';
+        }
+    })
 }
