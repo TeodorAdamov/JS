d5fabd2bad4452e281dc6568ae20607d40293392
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2023-10-04 15:46:06 +0300

    classes 04 and 05

---
+++ advanced/advanced/classes/06. Company/company.js
@@ -0,0 +1,58 @@
+class Company {
+    constructor() {
+        this.departments = {};
+    }
+
+    addEmployee(name, salary, position, department) {
+        if (!(name && salary && position && department) || salary < 0) {
+            throw new Error('Invalid input!')
+        }
+        if (!this.departments.hasOwnProperty(department)) {
+            this.departments[department] = [];
+        }
+
+        this.departments[department].push({ name, salary, position });
+        return `New employee is hired. Name: ${name}. Position: ${position}`
+    }
+
+    bestDepartment() {
+        let highestAverageSalary = 0;
+        let department = ''
+
+        for (let key of Object.keys(this.departments)) {
+            let averageSalary = this.departments[key].reduce((acc, current) => acc + current.salary, 0) / this.departments[key].length;
+            if (highestAverageSalary < averageSalary) {
+                highestAverageSalary = averageSalary;
+                department = key;
+            }
+        }
+
+
+        const sortedDepartment = this.departments[department].sort((a, b) => b.salary - a.salary || a.name.localeCompare(b.name))
+        const output = []
+
+        sortedDepartment.forEach(worker => {
+            output.push(`${worker.name} ${worker.salary} ${worker.position}`)
+        })
+
+        
+
+        return`Best Department is: ${department}\nAverage salary: ${highestAverageSalary.toFixed(2)}\n${output.join('\n')}`
+    }
+}
+
+let c = new Company();
+c.addEmployee("Stanimir", 2000, "engineer", "Human resources");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+
+console.log(c.bestDepartment());
+
+
+Unexpected error: expected 
+'Best Department is: Human resources\nAverage salary: 1675.00\nStanimir 2000 engineer\nGosho 1350 HR' to equal 
+'Best Department is: Human resources\nAverage salary: 1675.00\nStanimir 2000 engineer\nGosho 1350 HR'
