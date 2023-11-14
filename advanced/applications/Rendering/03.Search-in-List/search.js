95730a0aa096dd6f3b612ed340b6889e4d660bd2
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2023-11-14 10:30:33 +0200

    rendering exercise 3

--- advanced/applications/Rendering/03.Search-in-List/search.js
+++ advanced/applications/Rendering/03.Search-in-List/search.js
@@ -1,3 +1,27 @@
-function search() {
-   // TODO
+import { towns } from "./towns.js";
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+document.getElementById('towns').appendChild(document.createElement('ul'))
+
+//render towns
+const townTemplate = (town) => html`<li>${town}</li>`
+render(towns.map(townTemplate), document.querySelector('ul'))
+
+//get all li's as an array
+const liArray = Array.from(document.querySelectorAll('li'));
+
+//event listener
+document.querySelector('button').addEventListener('click', (event) => search(event, liArray))
+
+
+
+function search(event, liArr) {
+    const readInput = document.getElementById('searchText');
+    if (!readInput.value) {
+        alert('You must fill the search field!')
+    }
+    const filtered = liArr.filter(el => el.textContent.includes(readInput.value))
+    liArr.forEach(el => el.className = '')
+    filtered.forEach(el => el.className = 'active')
+    document.getElementById('result').textContent = `${filtered.length} matches found`
 }
