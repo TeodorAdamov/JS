ac7b8669d9f4aaaa7bf737b7673fe7c0fbf5123d
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2023-11-14 11:46:54 +0200

    rendering exercise 4

--- advanced/applications/Rendering/04.Fill-Dropdown/dropdown.js
+++ advanced/applications/Rendering/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,34 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from './node_modules/lit-html/lit-html.js'
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown'
+window.addEventListener('DOMContentLoaded', dropDownFiller);
+
+const dropDownTemplate = (item) => html`<option value="${item._id}">${item.text}</option>`
+
+document.querySelector('form').addEventListener('submit', onSubmit)
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const text = document.getElementById('itemText');
+    if (!text.value) {
+        alert('You must fill the empty text field!')
+    }
+
+    await fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: text.value })
+    })
+    dropDownFiller()
+}
+
+async function dropDownFiller() {
+    return render(Object.values(await getItems()).map(dropDownTemplate), document.getElementById('menu'))
+}
+
+async function getItems() {
+    const res = await fetch(url);
+    const data = await res.json()
+    return data
 }
